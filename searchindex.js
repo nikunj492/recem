db6bfdadb3391c095b22f39b70a8d80f3e86b22d
Shoibal Chakravarty <shoibalc@gmail.com>
2012-04-12 11:13:49 -0400

    saving docs.back to master

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,recem:0,modul:0,indic:0,content:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to Recem&#8217;s documentation!"],objnames:{},filenames:["index"]})
+Search.setIndex({objects:{},terms:{index:0,search:0,welcom:[],climat:0,recem:0,modul:0,econom:0,indic:0,page:0,content:0,repositori:0,tabl:0,model:0,document:[],energi:0},objtypes:{},titles:["Recem: Repository of Energy, Climate and Economics Models"],objnames:{},filenames:["index"]})
